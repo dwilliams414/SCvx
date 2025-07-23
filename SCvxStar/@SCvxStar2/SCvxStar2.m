76e37d150f2eef56bb12757be71b2bf376ec27e0
Dale W <dale@pwdac.com>
2025-07-23 10:12:18 -0400

    Implemented option to solve using explicit cone problem.

--- SCvxStar/@SCvxStar2/SCvxStar2.m
+++ SCvxStar/@SCvxStar2/SCvxStar2.m
@@ -237,7 +237,187 @@ classdef SCvxStar2
             zOpt = value(zSDP);
         end
 
-        zOpt = solveFast(obj, zInit);
+        function zOpt = solveCone(obj, zInit)
+            %solveStandard: Solve the optimization problem using the SCvx*
+            %framework (slow building, no optimizer object in YALMIP).
+            %Uses a cone problem formulation identical to that in
+            %SCvxStarFast
+            %   Inputs:
+            %       zInit:      Initial guess for optimization variables
+            %   Outputs:
+            %       zOpt:       Optimized solution
+            arguments
+                obj (1, 1) SCvxStar2
+                zInit (:, 1) double
+            end
+            % Set Current Values for Quantities:
+            zRef = zInit;
+            currentW = obj.wInit;
+            currentTR = obj.initTRRadius;
+
+            optMetric = inf;
+            feasMetric = inf;
+            delta = inf; % delta from SCVx*
+
+            numIter = 0;
+
+            % Get initial g_lin, h_lin to size lagrange multipliers
+            glinInit = obj.cvxSubproblem.g_linearized(zRef, zRef);
+            hlinInit = obj.cvxSubproblem.h_linear(zRef, zRef);
+
+            currentLambda = zeros(size(glinInit));
+            currentMu = zeros(size(hlinInit));
+
+            while (optMetric > obj.tolOpt || feasMetric > obj.tolFeas) && ...
+                    numIter < obj.maxIter
+
+                numIter = numIter + 1;
+                yalmip('clear'); % Clear existing SDP variables
+
+                % Build the optimization variable vector
+                zSDP = obj.cvxSubproblem.build_sdpvar(zRef);
+
+                % Generate the linearized constraints and convexified
+                % inequality constraints
+                gTilde = obj.cvxSubproblem.g_linearized(zSDP, zRef);
+                hTilde = obj.cvxSubproblem.h_linear(zSDP, zRef);
+
+                % Generate the Affine and Convex constraints
+                gAffine = obj.cvxSubproblem.g_affine(zSDP);
+                hConvex = obj.cvxSubproblem.h_cvx(zSDP);
+
+                % Initialize the (Subproblem) constraint vector
+                constraints = [gAffine == 0; hConvex <= 0];
+
+                if ~isempty(gTilde)
+                    xi = sdpvar(length(gTilde), 1);
+                    constraints = [constraints; gTilde - xi == 0];
+                    lambdaSDP = sdpvar(length(xi), 1);
+                    t1 = sdpvar(1);
+                    constraints = [constraints; 1e4*norm([2*xi; t1-1], 2) <= 1e4*(t1+1); t1>=0];
+                else
+                    xi = [];
+                    lambdaSDP = [];
+                    t1 = 0;
+                end
+    
+                if ~isempty(hTilde)
+                    zeta = sdpvar(length(hTilde), 1);
+                    constraints = [constraints; hTilde - zeta <= 0];
+                    constraints = [constraints; zeta >= 0];
+                    muSDP = sdpvar(length(zeta), 1);
+                    t2 = sdpvar(1);
+                    constraints = [constraints; norm([2*zeta; t2-1], 2) <= t2+1; t2>=0];
+                else
+                    zeta = [];
+                    muSDP = [];
+                    t2 = 0;
+                end
+
+                % Append Trust Region Constraints
+                constraints = [constraints; norm(zSDP-zRef, inf) <= currentTR];
+
+                % Solve the CVX Subproblem
+                L = obj.augmentedLagrangianSDPCone(zSDP, xi, zeta, currentW, ...
+                    currentLambda, currentMu, t1, t2);
+
+                opts = sdpsettings('solver', 'mosek');
+                opts.mosek.MSK_DPAR_BASIS_TOL_X = 1e-9;
+                opts.mosek.MSK_DPAR_BASIS_TOL_S = 1e-9;
+                opts.mosek.MSK_DPAR_INTPNT_CO_TOL_DFEAS = 1e-11;
+                opts.mosek.MSK_DPAR_INTPNT_CO_TOL_PFEAS = 1e-11;
+                opts.mosek.MSK_DPAR_INTPNT_CO_TOL_REL_GAP = 1e-11;
+
+                sol = optimize(constraints, L, opts);
+
+                % Evaluate Nonlinear Constraints - Current Iteration
+                gCurrent = obj.cvxSubproblem.g_nonlinear(value(zSDP));
+                hCurrent = obj.cvxSubproblem.h_nonlinear(value(zSDP));
+                Jcurrent = obj.augmentedLagrangian(value(zSDP), ...
+                    gCurrent, hCurrent, currentW, currentLambda, ...
+                    currentMu);
+
+                % Evaluate Nonlinear Constraints - Previous Iteration
+                gPrev = obj.cvxSubproblem.g_nonlinear(zRef);
+                hPrev = obj.cvxSubproblem.h_nonlinear(zRef);
+                Jprev = obj.augmentedLagrangian(zRef, gPrev, hPrev, ...
+                    currentW, currentLambda, currentMu);
+                
+                % Cost Function Deltas
+                deltaJCurrent = Jprev-Jcurrent;
+                deltaLCurrent = Jprev-value(L);
+                optMetric = abs(deltaJCurrent); % Optimality metric
+
+                if (deltaLCurrent < 0)
+                    warning("This shouldn't be negative...attempt to " + ...
+                        "diagnose!");
+                end
+                
+                % Evaluate Feasibility
+                feasVector = [gCurrent; max(0, hCurrent)];
+                feasMetric = norm(feasVector, inf);
+
+                % Evaluate step improvement
+                if deltaLCurrent == 0
+                    currentRho = 1;
+                else
+                    currentRho = deltaJCurrent/deltaLCurrent;
+                end
+
+                fprintf("Step Results, Iteration %d:\n", numIter);
+                fprintf("Iteration Optimality (deltaJ): %.4g\n", deltaJCurrent);
+                fprintf("Iteration Feasibility: %.4g\n", feasMetric);
+                fprintf("Iteration deltaL: %.4g\n", deltaLCurrent);
+                fprintf("Current Trust Region: %.4g\n", currentTR);
+                fprintf("Current rho: %.4g\n", currentRho);
+                fprintf("Current w: %.4g\n", currentW);
+                fprintf("Current Optimality Criteria to Update (delta): %.4g\n", delta);
+                fprintf("YALMIP Time: %.4g\n", sol.yalmiptime)
+
+                % Assess Step Improvement - keep results
+                if currentRho >= obj.rho0
+                    zRef = value(zSDP); % Update the reference...good step
+
+                    % Assess if we should update weights - we have improved
+                    % optimality enough
+                    if abs(deltaJCurrent) < delta
+                        fprintf("Updated Multipliers!\n");
+                        % Update Multipliers
+                        currentLambda = currentLambda + currentW*gCurrent;
+                        currentMu = max(0, currentMu + currentW*hCurrent);
+                        currentW = min(obj.beta*currentW, obj.wMax);
+
+                        % Update Stationary Tolerance
+                        if isinf(delta)
+                            delta = abs(deltaJCurrent);
+                        else
+                            delta = obj.gamma * delta;
+                        end
+                    end
+                end
+
+                % Update Trust Region
+                if currentRho < obj.rho1
+                    fprintf("Reduced Trust Region\n");
+                    currentTR = max(currentTR/obj.alpha1, obj.rMin);
+                elseif currentRho > obj.rho2
+                    fprintf("Expanded Trust Region\n");
+                    currentTR = min(obj.alpha2*currentTR, obj.rMax);
+                end
+            end
+            zOpt = value(zSDP);
+        end
+
+        % zOpt = solveFast(obj, zInit);
+        
+        function L = augmentedLagrangianSDPCone(obj, zSDP, xi, zetsa, w, ...
+                lambda, mu, t1, t2)
+            f0 = obj.cvxSubproblem.cost_fcn(zSDP);
+            f0 = obj.cvxSubproblem.cost_fcn(zSDP);
+
+            L = f0 + dot(lambda, xi) + w/2 * t1 + dot(mu, zeta) + ...
+               w/2 * t2; % dot(zeta, zeta) because we constraint zeta >= 0!
+        end
 
         function L = augmentedLagrangianSDP(obj, zSDP, xi, zeta, w, lambda, mu)
             % AUGMENTEDLAGRANGIANSDP: Augmented lagrangian that for which
